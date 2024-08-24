204a933c04c5b7dd0187b52f77ac10d629603af6
YulianaHrynda <yuliana.lpg1809@gmail.com>
2024-08-24 11:10:46 +0300

    created creation of room and Room model

--- roommate/urls.py
+++ roommate/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("profile/", TemplateView.as_view(template_name="profile.html"), name="profile"),
+    path("room/", include("room.urls")),
 ]
